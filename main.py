2d244f2ff70d8cb84c06d7a2cfeb19c603d3809e
karanautomation25 <karanautomation25@gmail.com>
2024-02-18 01:33:32 +0530

    added sliding window programs

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-# https://takeuforward.org/data-structure/maximum-product-subarray-in-an-array/
+A = [4, 1, 1, 1, 2, 3, 5]
+k = 5
 
-Nums = [1,2,3,4,5,0]
+n = len(A)
+
+i = 0
+j = 0
+sum = 0
+max_size = 0
+while j < n:
+    sum = sum + A[j]
+    if sum < k:
+        j+=1
+
+    elif sum == k:
+        size = (j-1)-i+1
+        max_size = max(max_size,size)
+        j+=1
+
+
+    i+=1
+    print('hey')
+
